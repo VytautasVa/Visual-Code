b7ac79e87dd65e1fed89e2c03d8016ac83cc01ee
VytautasVa <el.pastas@gmail.com>
2023-12-22 21:30:36 +0200

    up

--- Mano/JS101/JSa/turinioIšvedimas.js
+++ Mano/JS101/JSa/turinioIšvedimas.js
@@ -1,16 +1,22 @@
-//turinys į ekraną, naujai atidarant
-alert("😁 textas 🤯")
+// //turinys į ekraną, naujai atidarant
+// alert("😁 textas 🤯")
 
-//turinys į erroro skilį
-console.error("mano erroro textas 🤔")
+// //turinys į erroro skilį
+// console.error("mano erroro textas 🤔")
 
-//turinys į warning skilį
-console.warn("mano Warning textas 😊");
+// //turinys į warning skilį
+// console.warn("mano Warning textas 😊");
 
-//turinys į console
-console.log("Teksas į Console 🧑‍💻")
+// //turinys į console
+// console.log("Teksas į Console 🧑‍💻")
 
-//turinys į naršyklės ekraną
-document.write("tekstas 👍");
+// //turinys į naršyklės ekraną
+// document.write("tekstas 👍");
+
+// let teksatas = document.querySelector(".text");
+// teksatas.innerHTML = "<b>Naujas 😜 tekstas</b>";
 
 //norint patikrinti duomenų tipą // console.log(typeof tikrinamas turinys) //
+
+document.querySelector(".text").innerHTML = "<b>Naujas tekstas</b>";
+document.querySelector(".text2").innerText = "<b>Naujas tekstas</b>";
