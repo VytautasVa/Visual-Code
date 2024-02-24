9fb5d80175a02af48ea9976abc8fd62ecb4efaa2
VytautasVa <vytautasv85@gmail.com>
2024-02-24 19:08:42 +0200

    up

--- React/in-work/src/App.js
+++ React/in-work/src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import Accordion from './Components/Accordion';
 import RandomColor from './Components/random-color';
 import StarRaiting from './Components/star-raiting';
-import ImageSlider from './image-slider';
+import ImageSlider from './Components/image-slider';
+import LoadMoreData from './Components/load-more-data';
+import TreeView from './Components/tree-view';
+import menus from './Components/tree-view/data';
+import QRCodeGenerator from './Components/QR-code-generator';
 
 function App() {
   return (
     <div className="App">
-      {/* Accordion component */}
-      {/* <Accordion/> */}
-      {/* Random color component */}
-      {/* <RandomColor/> */}
-      {/* Star raiting component */}
-      {/* <StarRaiting noOfStars={10}/> */}
-      {/* Image slider component */}
+      {/* <Accordion/>
+      <RandomColor/>
+      <StarRaiting noOfStars={10}/>
       <ImageSlider 
         url={"https://picsum.photos/v2/list"} 
         pages={'1'} 
         limit={'4'}/>
+      <LoadMoreData/> */}
+      {/* <TreeView menus={menus}/> */}
+      <QRCodeGenerator/>
     </div>
   );
 }
