f7089a97c12d0cddc3a24c96225971e9152ed02d
VytautasVa <vytautasv85@gmail.com>
2024-02-29 20:36:32 +0200

    timer

--- React/timer/src/components/Countdowntimer.jsx
+++ React/timer/src/components/Countdowntimer.jsx
@@ -1,6 +1,8 @@
 import React, { useState ,useEffect } from "react";
 import Timer from "./Timer";
+import {BsFillPlayFill, BsPauseFill, BsStopFill} from "react-icons/bs";
 
+document.body.style.background = "#282c50"
 
 export default function CountDownTimer(){
     const[hours, setHours] = useState(0);
@@ -8,6 +10,11 @@ export default function CountDownTimer(){
     const[seconds, setSeconds] = useState(0);
     const[miliseconds, setMiliseconds] = useState(0);
     const[isRunning, setIsRunning] = useState(null);
+
+    const[showEndScrean, setShowEndScrean] = useState({
+        show: false,
+        message: "Happy coding in 2024 !!!"
+    });
 
     useEffect(()=>{
         let interval;
@@ -30,14 +37,79 @@ export default function CountDownTimer(){
                 }
             }, 10);
         }
+        if(hours === 0 && minutes === 0 && seconds === 0 && miliseconds === 1){
+            setShowEndScrean({...showEndScrean, show: true});
+            resetTimer();
+        }
+
         return () => clearInterval(interval);
-    },[miliseconds, seconds, minutes, hours, isRunning]);
+    },[miliseconds, seconds, minutes, hours, isRunning, showEndScrean.show]);
+
+    function startTimer(){
+        if(hours !== 0 || minutes !== 0 || seconds !== 0 || miliseconds !== 0){
+            setIsRunning(true);
+            setShowEndScrean({...showEndScrean, show: false});
+        }else{
+            window.alert("Reikia įvesti laiką")
+        }
+    }
+
+    function pauseTimer(){
+        setIsRunning(false);
+    }
+
+    function stopTimer(){
+        resetTimer();
+        setShowEndScrean({...showEndScrean, show: false});
+    }
+
+    function resetTimer(){
+        setIsRunning(false);
+        setMiliseconds(0);
+        setSeconds(0);
+        setMinutes(0);
+        setHours(0);
+    }
+
+    const changeSeconds = (e) => {
+        setSeconds(e.target.value);
+    };
+    const changeMinutes = (e) => {
+        setMinutes(e.target.value);
+    };
+    const changeHours = (e) => {
+        setHours(e.target.value);
+    };
 
     return (
         <div>
-            <h1 className='title'>Happy coding in 2024 !!!</h1>
+            {showEndScrean.show && (
+                <h1 className='title text-light'>{showEndScrean.message}</h1>
+                )}
 
-            <Timer/>
+            <Timer
+                miliseconds={miliseconds} 
+                seconds={seconds} 
+                minutes={minutes} 
+                hours={hours}
+                changeSeconds={changeSeconds}
+                changeMinutes={changeMinutes}
+                changeHours={changeHours}
+                />
+
+            <br />
+
+            {!isRunning && (
+                <button className="btn btn-accept btn-lg" onClick={startTimer}>
+                    <BsFillPlayFill/>
+                </button>)}
+            {isRunning && (
+                <button className="btn btn-warning btn-lg" onClick={pauseTimer}>
+                    <BsPauseFill/>
+                </button>)}{" "}
+            <button className="btn btn-danger btn-lg" onClick={stopTimer}>
+                <BsStopFill/>
+            </button>
         </div>
     );
 }
--- React/timer/src/components/Timer.jsx
+++ React/timer/src/components/Timer.jsx
@@ -15,27 +15,52 @@ const TimerWrapper = styled.div`
     justify-content: center;
     box-shadow: 5px 4px 6px rgba(0,0,0,0.4);
     padding: 1rem 0;
+
+    .stop-watch{
+        font-size: 6rem;
+        margin-right: 1rem;
+    }
+    label{
+        margin-bottom: 0.5rem;
+    }
+    input{
+        width: 100px;
+        margin-right: 1rem;
+        color: #282c34;
+        outline: none;
+        border: none;
+        font-size: 4.5rem;
+        font-weight: 600;
+        text-align: center;
+        padding: 0rem 0.5rem;
+        border-radius: 5px;
+    }
+    input:hover{
+        background-color: #928f8f;
+    }
     `
 
-export default function Timer(){
+export default function Timer({
+    miliseconds, seconds, minutes, hours,
+    changeSeconds, changeMinutes, changeHours}){
     return(
         <TimerWrapper>
-            <BsStopwatch/>
+            <BsStopwatch className="stop-watch"/>
             <div className="d-flex flex-column">
                 <label>hh</label>
-                <input value={0}/>
+                <input value={hours} onChange={changeHours}/>
             </div>
             <div className="d-flex flex-column">
                 <label>mm</label>
-                <input value={0}/>
+                <input value={minutes} onChange={changeMinutes}/>
             </div>
             <div className="d-flex flex-column">
                 <label>ss</label>
-                <input value={0}/>
+                <input value={seconds} onChange={changeSeconds}/>
             </div>
             <div className="d-flex flex-column">
                 <label>ms</label>
-                <input value={0}/>
+                <input value={miliseconds}/>
             </div>
         </TimerWrapper>
     )
