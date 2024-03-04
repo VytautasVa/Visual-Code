d6e9ac59ac07a7152e3d6f91c5179c7047f244cb
VytautasVa <vytautasv85@gmail.com>
2024-03-04 11:34:48 +0200

    timer

--- React/timer/src/index.js
+++ React/timer/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CountDownTimer from './components/Countdowntimer';
 import './index.css';
+import StopWatch from './components/Stopwatch';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,7 @@ root.render(
   <React.StrictMode>
     <div className="App">
       <CountDownTimer/>
+      <StopWatch/>
     </div>
   </React.StrictMode>
 );
